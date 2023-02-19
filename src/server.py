d42baacf75f0f49206cd69680df902b99b4982a8
happymonkey1 <brownejamessean@gmail.com>
2023-02-18 18:02:02 -0800

    update endpoints to include v1

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ import flask
 from flask import Flask
 import typing
 import src.user
+import src.recommendation_engine as engine
 
 # create flask app at a module level
 flask_app = Flask(__name__) 
@@ -17,18 +18,22 @@ class macrofi_server():
         return self.instance
     
     """initialize the instance"""
-    def init(self, in_memory_user_cache: typing.Dict[int, src.user.user_profile_data], threaded: bool = False):
+    def init(self, in_memory_user_cache: typing.Dict[int, src.user.user_profile_data], threaded: bool = False, port: int=5000):
         # flag for whether we should run in threaded mode or not
         self.__threaded = threaded
+        # port to run the server on
+        self.__port = port
         # user data in memory cache
         self.__in_memory_user_cache: typing.Dict[int, src.user.user_profile_data] = in_memory_user_cache
+        # cache of active user recommendation engines
+        self.__active_user_recommendation_engines: typing.Dict[int, engine.recommendation_engine] = {}
         
         return self
     
     """initialize and run the flask server"""
     def run(self):
         # TODO(Sean): figure out how to run out of debug mode
-        flask_app.run(host="0.0.0.0", debug=True, threaded=self.__threaded)
+        flask_app.run(host="0.0.0.0", debug=True, threaded=self.__threaded, port=self.__port)
     
     """internal method to check user cache and return a response"""
     def __get_user_data(self, uuid: int):
@@ -59,12 +64,12 @@ class macrofi_server():
         # TODO(Sean): return as json
         return str(found_user._meals)
     
-"""get api call for /user/<uuid> to return user profile data"""
-@flask_app.get("/user/<uuid>")
+"""get api call for /v1/user/<uuid> to return user profile data"""
+@flask_app.get("/v1/user/<uuid>")
 def get_user_data(uuid: int):
     return macrofi_server()._macrofi_server__get_user_data(uuid=uuid)
 
-"""get api call for /user/meals/<uuid> to cached meals from the specified user"""
-@flask_app.get("/user/meals/<uuid>")
+"""get api call for /v1/user/meals/<uuid> to cached meals from the specified user"""
+@flask_app.get("/v1/user/meals/<uuid>")
 def get_user_meal_data(uuid: int):
     return macrofi_server()._macrofi_server__get_user_meal_data(uuid=uuid)
